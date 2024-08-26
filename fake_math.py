8f42c5d94f2fcab948ba7fe6ce25fec0360c4525
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-26 12:40:31 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'  # print('Ошибка') - выводит сперва текст, а потом значение None
+    else:
+        return first / second
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as calc_1
+from true_math import divide as calc_2
+
+result1 = calc_1(69, 3)
+result2 = calc_1(3, 0)
+result3 = calc_2(49, 7)
+result4 = calc_2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
